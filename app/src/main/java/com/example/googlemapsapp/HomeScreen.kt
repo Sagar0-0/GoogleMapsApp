5d6ae3a0c0b99a05f8ccf641da03d334abb84db1
Sagar <sagar.0dev@gmail.com>
2023-03-22 10:33:19 +0530

    blog

--- app/src/main/java/com/example/googlemapsapp/HomeScreen.kt
+++ app/src/main/java/com/example/googlemapsapp/HomeScreen.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -28,31 +29,30 @@ import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
 import com.google.maps.android.compose.*
 
 
-private var locationPermissionGranted = mutableStateOf(false)
-private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1234
+//private var locationPermissionGranted = mutableStateOf(false)
+//private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1234
 
-@OptIn(MapsComposeExperimentalApi::class)
 @SuppressLint("MissingPermission")
 @Composable
 fun HomeScreen() {
     val context = LocalContext.current
-    val placesClient = Places.createClient(context)
-
-    var likelyPlaceNames by remember {
-        mutableStateOf<Array<String?>>(arrayOfNulls(0))
-    }
-    var likelyPlaceAddresses by remember {
-        mutableStateOf<Array<String?>>(arrayOfNulls(0))
-    }
-    var likelyPlaceAttributions by remember {
-        mutableStateOf<Array<List<*>?>>(arrayOfNulls(0))
-    }
-    var likelyPlaceLatLngs by remember {
-        mutableStateOf<Array<LatLng?>>(arrayOfNulls(0))
-    }
-    var count by remember {
-        mutableStateOf(0)
-    }
+//    val placesClient = Places.createClient(context)
+//
+//    var likelyPlaceNames by remember {
+//        mutableStateOf<Array<String?>>(arrayOfNulls(0))
+//    }
+//    var likelyPlaceAddresses by remember {
+//        mutableStateOf<Array<String?>>(arrayOfNulls(0))
+//    }
+//    var likelyPlaceAttributions by remember {
+//        mutableStateOf<Array<List<*>?>>(arrayOfNulls(0))
+//    }
+//    var likelyPlaceLatLngs by remember {
+//        mutableStateOf<Array<LatLng?>>(arrayOfNulls(0))
+//    }
+//    var count by remember {
+//        mutableStateOf(0)
+//    }
 
     val fusedLocationProviderClient =
         remember { LocationServices.getFusedLocationProviderClient(context) }
@@ -69,84 +69,80 @@ fun HomeScreen() {
         position = CameraPosition.fromLatLngZoom(deviceLatLng, 18f)
     }
 
-    try {
-        val locationResult = fusedLocationProviderClient.lastLocation
-        locationResult.addOnCompleteListener(context as MainActivity) { task ->
-            if (task.isSuccessful) {
-                // Set the map's camera position to the current location of the device.
-                lastKnownLocation = task.result
-                deviceLatLng = LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude)
-                cameraPositionState.position = CameraPosition.fromLatLngZoom(deviceLatLng, 18f)
-            } else {
-                Log.d(TAG, "Current location is null. Using defaults.")
-                Log.e(TAG, "Exception: %s", task.exception)
-            }
-        }
-    } catch (e: SecurityException) {
-        Log.e("Exception: %s", e.message, e)
-    }
-
-    // Use fields to define the data types to return.
-    val placeFields = listOf(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG)
-
-    // Use the builder to create a FindCurrentPlaceRequest.
-    val request = FindCurrentPlaceRequest.newInstance(placeFields)
-
-    // Get the likely places - that is, the businesses and other points of interest that
-    // are the best match for the device's current location.
-    val placeResult = placesClient.findCurrentPlace(request)
-    placeResult.addOnCompleteListener { task ->
-        if (task.isSuccessful && task.result != null) {
-            val likelyPlaces = task.result
-
-            // Set the count, handling cases where less than 5 entries are returned.
-            count =
-                if (likelyPlaces != null && likelyPlaces.placeLikelihoods.size < M_MAX_ENTRIES) {
-                    likelyPlaces.placeLikelihoods.size
-                } else {
-                    M_MAX_ENTRIES
-                }
-            var i = 0
-            likelyPlaceNames = arrayOfNulls(count)
-            likelyPlaceAddresses = arrayOfNulls(count)
-            likelyPlaceAttributions = arrayOfNulls<List<*>?>(count)
-            likelyPlaceLatLngs = arrayOfNulls(count)
-            for (placeLikelihood in likelyPlaces?.placeLikelihoods ?: emptyList()) {
-                // Build a list of likely places to show the user.
-                likelyPlaceNames[i] = placeLikelihood.place.name
-                likelyPlaceAddresses[i] = placeLikelihood.place.address
-                likelyPlaceAttributions[i] = placeLikelihood.place.attributions
-                likelyPlaceLatLngs[i] = placeLikelihood.place.latLng
-                i++
-                if (i > count - 1) {
-                    break
-                }
-            }
+    val locationResult = fusedLocationProviderClient.lastLocation
+    locationResult.addOnCompleteListener(context as MainActivity) { task ->
+        if (task.isSuccessful) {
+            // Set the map's camera position to the current location of the device.
+            lastKnownLocation = task.result
+            deviceLatLng = LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude)
+            cameraPositionState.position = CameraPosition.fromLatLngZoom(deviceLatLng, 18f)
         } else {
+            Log.d(TAG, "Current location is null. Using defaults.")
             Log.e(TAG, "Exception: %s", task.exception)
         }
     }
 
+    // Use fields to define the data types to return.
+//    val placeFields = listOf(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG)
+//
+//    // Use the builder to create a FindCurrentPlaceRequest.
+//    val request = FindCurrentPlaceRequest.newInstance(placeFields)
+//
+//    // Get the likely places - that is, the businesses and other points of interest that
+//    // are the best match for the device's current location.
+//    val placeResult = placesClient.findCurrentPlace(request)
+//    placeResult.addOnCompleteListener { task ->
+//        if (task.isSuccessful && task.result != null) {
+//            val likelyPlaces = task.result
+//
+//            // Set the count, handling cases where less than 5 entries are returned.
+//            count =
+//                if (likelyPlaces != null && likelyPlaces.placeLikelihoods.size < M_MAX_ENTRIES) {
+//                    likelyPlaces.placeLikelihoods.size
+//                } else {
+//                    M_MAX_ENTRIES
+//                }
+//            var i = 0
+//            likelyPlaceNames = arrayOfNulls(count)
+//            likelyPlaceAddresses = arrayOfNulls(count)
+//            likelyPlaceAttributions = arrayOfNulls<List<*>?>(count)
+//            likelyPlaceLatLngs = arrayOfNulls(count)
+//            for (placeLikelihood in likelyPlaces?.placeLikelihoods ?: emptyList()) {
+//                // Build a list of likely places to show the user.
+//                likelyPlaceNames[i] = placeLikelihood.place.name
+//                likelyPlaceAddresses[i] = placeLikelihood.place.address
+//                likelyPlaceAttributions[i] = placeLikelihood.place.attributions
+//                likelyPlaceLatLngs[i] = placeLikelihood.place.latLng
+//                i++
+//                if (i > count - 1) {
+//                    break
+//                }
+//            }
+//        } else {
+//            Log.e(TAG, "Exception: %s", task.exception)
+//        }
+//    }
+
     Column(Modifier.fillMaxSize()) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .background(Color.Green)
-                .padding(10.dp)
-                .fillMaxWidth()
-                .height(34.dp)
-        ) {
-            Icon(
-                modifier = Modifier.size(24.dp),
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = ""
-            )
-            Spacer(modifier = Modifier.width(6.dp))
-            Text(text = "Location", modifier = Modifier.weight(1f), fontSize = 24.sp)
-        }
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically,
+//            modifier = Modifier
+//                .background(Color.Green)
+//                .padding(10.dp)
+//                .fillMaxWidth()
+//                .height(34.dp)
+//        ) {
+//            Icon(
+//                modifier = Modifier.size(24.dp),
+//                imageVector = Icons.Default.ArrowBack,
+//                contentDescription = ""
+//            )
+//            Spacer(modifier = Modifier.width(6.dp))
+//            Text(text = "Location", modifier = Modifier.weight(1f), fontSize = 24.sp)
+//        }
 
         GoogleMap(
-            modifier = Modifier.weight(0.5f),
+//            modifier = Modifier.weight(0.5f),
             cameraPositionState = cameraPositionState
         ) {
             MarkerInfoWindowContent(
@@ -156,33 +152,38 @@ fun HomeScreen() {
             ) { marker ->
                 Text(marker.title ?: "You", color = Color.Red)
             }
-            for (i in 0 until count) {
-                MarkerInfoWindowContent(
-                    state = MarkerState(likelyPlaceLatLngs[i]!!)
-                ) {
-                    Text(likelyPlaceNames[i]!!, color = Color.Red)
-                }
-            }
+//            for (i in 0 until count) {
+//                MarkerInfoWindowContent(
+//                    state = MarkerState(likelyPlaceLatLngs[i]!!)
+//                ) {
+//                    Text(likelyPlaceNames[i]!!, color = Color.Red)
+//                }
+//            }
         }
 
-        LazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.LightGray)
-                .weight(0.5f)
-        ) {
-            items(count) { i ->
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = likelyPlaceNames[i]!!, fontSize = 24.sp
-                )
-                Spacer(
-                    modifier = Modifier
-                        .height(3.dp)
-                        .background(Color.Black)
-                )
-            }
-        }
+//        LazyColumn(
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(Color.LightGray)
+//                .weight(0.5f)
+//        ) {
+//            if (count == 0) {
+//                item {
+//                    CircularProgressIndicator()
+//                }
+//            }
+//            items(count) { i ->
+//                Text(
+//                    modifier = Modifier.padding(5.dp),
+//                    text = likelyPlaceNames[i]!!, fontSize = 24.sp
+//                )
+//                Spacer(
+//                    modifier = Modifier
+//                        .height(3.dp)
+//                        .background(Color.Black)
+//                )
+//            }
+//        }
     }
 }
--- app/src/main/java/com/example/googlemapsapp/MainActivity.kt
+++ app/src/main/java/com/example/googlemapsapp/MainActivity.kt
@@ -5,69 +5,61 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.input.pointer.PointerIconDefaults.Text
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.googlemapsapp.ui.theme.GoogleMapsAppTheme
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.libraries.places.api.Places
 
 
 class MainActivity : ComponentActivity() {
-    private var locationPermissionGranted =  mutableStateOf(false)
-    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1234
+    companion object {
+        const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1234
+    }
+
+    private val locationPermissionGranted = mutableStateOf(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Places.initialize(applicationContext, getString(R.string.MAPS_API_KEY))
 
         setContent {
-            GoogleMapsAppTheme {
-                if (locationPermissionGranted.value) {
-                    HomeScreen()
-                } else {
-                    getLocationPermission()
-                }
+            getLocationPermission()
+            if (locationPermissionGranted.value) {
+                HomeScreen()
+            }else{
+                Text("Need permission")
             }
         }
     }
+
 
     @Deprecated("Deprecated in Java")
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<String>,
+        permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when (requestCode) {
-            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty() &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
-                    locationPermissionGranted.value = true
-                }
-            }
-            else -> {
-                locationPermissionGranted.value = false
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode== PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            locationPermissionGranted.value=true
         }
     }
 
-
     private fun getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
-
-        ActivityCompat.requestPermissions(
-            this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-        )
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted.value = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
     }
 
 }
